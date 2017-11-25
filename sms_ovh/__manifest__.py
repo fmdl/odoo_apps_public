59ee00b2c7421493f88259d23d73823dbd30fe5c
Florent de Labarre <florent.mirieu@gmail.com>
2017-11-25 14:05:58 +0100

    INIT OVH SMS

---
+++ sms_ovh/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'OVH SMS Client',
+    'version': '2.0',
+    'license': 'AGPL-3',
+    'depends': ['sms', ],
+    'author': 'Florent de Labarre',
+    'website': 'https://github.com/fmdl',
+    'category': 'crm',
+    'external_dependencies': {'python': ['ovh'], },
+    'data': ['views/sms_views.xml', 'data/data.xml'],
+    'installable': True,
+    'price': 0.0,
+    'currency': 'EUR'
+}

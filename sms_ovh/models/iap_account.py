27c5b8535b24492a9bf083725685675c761b4f2f
Cyril VINH-TUNG <cyril@invitu.com>
2020-06-26 13:50:17 -1000

    [IMP] Add OVH SMS API User

--- sms_ovh/models/iap_account.py
+++ sms_ovh/models/iap_account.py
@@ -3,6 +3,7 @@
 from odoo import api, models, fields, _
 from odoo.exceptions import UserError
 
+
 class IapAccount(models.Model):
     _inherit = 'iap.account'
 
@@ -11,4 +12,5 @@ class IapAccount(models.Model):
     ovh_application_secret = fields.Char('Application secret')
     ovh_consumer_key = fields.Char('Consumer key')
     ovh_sms_account = fields.Char('SMS account')
+    ovh_sms_api_user = fields.Char('SMS API user', help='It can be empty if you do not use OVH API User')
     ovh_sender = fields.Char('Sender')
